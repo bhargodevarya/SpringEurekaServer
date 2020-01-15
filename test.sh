9100fa3d462ed3c7ca56537e11be822a6eacd491
Bhargodev Arya <bhargodevarya@gmail.com>
2020-01-15 13:09:18 +0530

    adding build script and updating travis ci

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+DOCKER_HUB_ID=$1
+DOCKER_HUB_PWD=$2
+tag="bhargodevarya/repo:eureka-"
+
+echo "starting the script"
+commit=$(git rev-parse HEAD)
+tag+="${commit:0:6}"
+
+echo "$tag"
+
+./gradlew check :EurekaServer:bootJar
+docker image build -t "$tag" ./EurekaServer
+docker login -u "$DOCKER_HUB_ID" -p "$DOCKER_HUB_PWD"
+docker image push "$tag"
+docker logout
+
+echo "Script finished"
